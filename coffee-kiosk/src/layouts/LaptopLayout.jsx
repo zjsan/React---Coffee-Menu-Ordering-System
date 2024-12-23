765d6538298aca82e7ed897d4affc9deb6f3d849
zjsan <zjsantos25@gmail.com>
2024-12-23 13:36:06 +0800

    updated code in the index.css and different layout

---
+++ coffee-kiosk/src/layouts/LaptopLayout.jsx
@@ -0,0 +1,13 @@
+import Index from "../components/Index"
+
+const LaptopLayout = () => {
+  return (
+    <div className="p-8 max-w-5xl mx-auto">
+      <div className="flex">
+        <Index />
+      </div>
+    </div>
+  )
+}
+
+export default LaptopLayout
---
+++ coffee-kiosk/src/layouts/MobileLayout.jsx
@@ -0,0 +1,11 @@
+import Index from "../components/Index"
+
+const MobileLayout = () => {
+  return (
+    <div className="p-4 max-w-md mx-auto">
+        <Index />
+    </div>
+  )
+}
+
+export default MobileLayout
