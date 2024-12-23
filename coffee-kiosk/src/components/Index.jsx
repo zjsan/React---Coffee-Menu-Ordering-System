80a82a70b9e3adbee363a82e73ed918a0387c984
zjsan <zjsantos25@gmail.com>
2024-12-23 10:51:41 +0800

    created different layout components

---
+++ coffee-kiosk/src/components/Index.jsx
@@ -0,0 +1,11 @@
+
+const Index = () => {
+  return (
+    <div className="container h-screen flex flex-col justify-center landing">
+      <h1 className="text-2xl font-bold">Welcome to Coffee Kiosk Pro</h1>
+      <button className="bg-gray-900 py-3 px-13 border-0 text-white rounded-2xl cursor-pointer start-button">Start Ordering</button>
+    </div>
+  )
+}
+
+export default Index
