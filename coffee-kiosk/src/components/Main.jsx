9af90b50d5dee5f4864d3ee68f8874d71c19492d
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2024-12-24 16:40:59 +0800

    Update Main.jsx

--- coffee-kiosk/src/components/Main.jsx
+++ coffee-kiosk/src/components/Main.jsx
@@ -1,7 +1,8 @@
 const Main = () => {
     return (
-      <div className="h-screen bg-gray-50">
-        
+      <div className="max-h-fit bg-gray-50">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab facere eum quibusdam ex ea? Nesciunt odit reiciendis iste doloremque aspernatur sunt neque repellat! Error optio atque impedit necessitatibus excepturi? Dolorem.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque sunt et sit, illum aperiam soluta, officiis atque, magnam omnis voluptatum pariatur. Voluptates sequi soluta quibusdam vitae ab, atque minima debitis!
       </div>
     );
   };
