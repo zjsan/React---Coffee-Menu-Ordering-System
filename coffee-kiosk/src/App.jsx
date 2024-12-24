25309683a2f74271736b097985faef7605c91b77
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2024-12-24 15:13:16 +0800

    Update App.jsx

--- coffee-kiosk/src/App.jsx
+++ coffee-kiosk/src/App.jsx
@@ -1,31 +1,16 @@
-import { useMediaQuery } from "react-responsive";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MobileLayout from "./layouts/MobileLayout";
-import TabletLayout from "./layouts/TabletLayout";
-import LaptopLayout from "./layouts/LaptopLayout";
 import Landing from "./components/Landing";
 import Page from "./components/Page";
 
 export default function App() {
-  const isMobile = useMediaQuery({ maxWidth: 767 });
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
-  const isLaptop = useMediaQuery({ minWidth: 1025 });
 
-  const Layout = () => {
-    if (isMobile) return <MobileLayout />;
-    if (isTablet) return <TabletLayout />;
-    if (isLaptop) return <LaptopLayout />;
-  };
-  
   return (
     <Router>
-      <div className="bg-gray-100 m-0 p-0">
-        <Layout />
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/page" element={<Page />} />
         </Routes>
-      </div>
     </Router>
   );
 }
