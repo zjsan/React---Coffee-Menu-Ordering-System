fdf96a96392b57272a26137ff25b26847763aca0
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2024-12-24 12:01:51 +0800

    Update App.jsx

--- coffee-kiosk/src/App.jsx
+++ coffee-kiosk/src/App.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MobileLayout from "./layouts/MobileLayout";
 import TabletLayout from "./layouts/TabletLayout";
 import LaptopLayout from "./layouts/LaptopLayout";
-import LandingPage from "./components/LandingPage";
-import MenuPage from "./components/MenuPage";
+import Landing from "./components/Landing";
+import Page from "./components/Page";
 
 export default function App() {
   const isMobile = useMediaQuery({ maxWidth: 767 });
@@ -22,8 +22,8 @@ export default function App() {
       <div className="bg-gray-100">
         <Layout />
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/menu" element={<MenuPage />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/page" element={<Page />} />
         </Routes>
       </div>
     </Router>
