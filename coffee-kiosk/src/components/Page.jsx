ceb058a39a4365b299ccb61b4ccb7230b8b2a76e
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2024-12-25 11:23:44 +0800

    Update Page.jsx

--- coffee-kiosk/src/components/Page.jsx
+++ coffee-kiosk/src/components/Page.jsx
@@ -1,14 +1,24 @@
-import Header from './Header'
-import Footer  from './Footer'
-import Main from './Main'
+import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
+import Navigation from './Navigation';
+
 const Page = () => {
   return (
-    <>
-      <Header/> 
-      <Main/>
-      <Footer/>
-    </>
-  )
-}
+    <div className="lg:flex lg:h-screen">
+      {/* Sidebar (Navigation) */}
+      <div className="lg:max-w-xs lg:h-full lg:bg-gray-900">
+        <Header />
+        <Navigation />
+      </div>
 
-export default Page
+      {/* Main Content */}
+      <div className="lg:flex-1">
+        <Main />
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
