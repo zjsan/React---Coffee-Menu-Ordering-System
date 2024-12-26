67a044929d994c68593d9e001a349bb7a329569a
zjsan <zjsantos25@gmail.com>
2024-12-26 14:37:36 +0800

    Update Navigation.jsx

--- coffee-kiosk/src/components/Navigation.jsx
+++ coffee-kiosk/src/components/Navigation.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <nav className="bg-gray-100 border-b border-gray-300">
-      <ul className="flex justify-around text-gray-700 text-sm font-semibold py-2">
+    <nav className="bg-gray-100 border-b border-gray-300 lg:border-0 lg:bg-gray-900 lg:text-white">
+      <ul className="flex lg:flex-col lg:space-y-4 justify-around lg:justify-start text-gray-700 lg:text-white text-lg font-semibold py-2 lg:p-5">
         <li className="hover:text-yellow-400 cursor-pointer">
           <Link to="/specials">Specials</Link>
         </li>
