885a7af50d44990287305a5c4bb8e771675ce4bf
zjsan <zjsantos25@gmail.com>
2024-12-26 16:20:15 +0800

    Update Coffee.jsx

--- coffee-kiosk/src/components/Coffee.jsx
+++ coffee-kiosk/src/components/Coffee.jsx
@@ -1,13 +1,29 @@
+import MenuItem from "./MenuItem";
+
 const Coffee = () => {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4 text-center">Coffee</h1>
-      <div className="grid grid-cols-3 gap-4">
-        <div className="border rounded-lg p-4 text-center">Special 1</div>
-        <div className="border rounded-lg p-4 text-center">Special 2</div>
-        <div className="border rounded-lg p-4 text-center">Special 3</div>
+  const specialsMenu = [
+      { name: "Americano", image: "/images/promo-latte.jpg" },
+      { name: "Caramel Macchiato", image: "/images/discount-americano.jpg" },
+      { name: "Spanish Latte", image: "/images/discount-americano.jpg" },
+      { name: "Mocha", image: "/images/discount-americano.jpg" },
+      { name: "Latte", image: "/images/discount-americano.jpg" },
+      { name: "Dirty Matcha", image: "/images/discount-americano.jpg" },
+    ];
+  
+    return (
+      <div className="h-screen bg-gray-50">
+          <h1 className="text-2xl font-bold mb-4 text-center">Coffee</h1>
+          <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
+            {specialsMenu.map((item, index) => (
+              <MenuItem
+                key={index}
+                name={item.name}
+                image={item.image}
+                onClick={() => console.log(`${item.name} clicked`)}
+              />
+            ))}
+          </div>
       </div>
-    </div>
   )
 }
 
