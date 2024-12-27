b5dba56dd2da5b39f80676c56a1b0f9273feef28
zjsan <zjsantos25@gmail.com>
2024-12-27 14:05:06 +0800

    Update Specials.jsx

--- coffee-kiosk/src/components/Specials.jsx
+++ coffee-kiosk/src/components/Specials.jsx
@@ -38,6 +38,7 @@ const Specials = () => {
             <h3 className="font-bold text-lg">Hello!</h3>
             <p className="py-4">Press ESC key or click the button below to close</p>
             <div className="modal-action">
+              <button className="btn">Add to Cart</button>
               <button className="btn" onClick={closeModal}>Close</button>
             </div>
           </div>
