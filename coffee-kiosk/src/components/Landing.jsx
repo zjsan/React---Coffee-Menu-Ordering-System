e6ee34b093f0ac19ad48cb1fa61188bbab9e0629
zjsan <zjsantos25@gmail.com>
2024-12-27 08:51:24 +0800

    updated routing to specials after clicking button in landing page

--- coffee-kiosk/src/components/Landing.jsx
+++ coffee-kiosk/src/components/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => {
       <h1 className="text-2xl font-bold">Welcome to Coffee Kiosk Pro</h1>
       <button 
         className="bg-gray-900 py-4 px-5 border-0 text-white rounded-3xl cursor-pointer start-button" 
-        onClick={() => navigate("/page")}
+        onClick={() => navigate("page/specials")}
       >
         Start Ordering
       </button>
