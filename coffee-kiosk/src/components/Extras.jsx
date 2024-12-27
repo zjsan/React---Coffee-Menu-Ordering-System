9323b5ef571b092328731f461b0a4404d73863d0
zjsan <zjsantos25@gmail.com>
2024-12-27 09:17:00 +0800

    Update Extras.jsx

--- coffee-kiosk/src/components/Extras.jsx
+++ coffee-kiosk/src/components/Extras.jsx
@@ -1,14 +1,28 @@
+import MenuItem from "./MenuItem";
+
 const Extras = () => {
-  return (
-    <div>
-    <h1 className="text-2xl font-bold mb-4 text-center">Extras</h1>
-    <div className="grid grid-cols-3 gap-4">
-      <div className="border rounded-lg p-4 text-center">Special 1</div>
-      <div className="border rounded-lg p-4 text-center">Special 2</div>
-      <div className="border rounded-lg p-4 text-center">Special 3</div>
-    </div>
-  </div>
-  )
+  
+     const specialsMenu = [
+        { name: "Espresso Shot", image: "/images/promo-latte.jpg" },
+        { name: "Syrup", image: "/images/discount-americano.jpg" },
+        { name: "Pancake", image: "/images/discount-americano.jpg" },
+        
+      ];
+    return (
+         <div className="h-screen bg-gray-50">
+             <h1 className="text-2xl font-bold mb-4 text-center">Non-Coffee</h1>
+             <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
+               {specialsMenu.map((item, index) => (
+                 <MenuItem
+                   key={index}
+                   name={item.name}
+                   image={item.image}
+                   onClick={() => console.log(`${item.name} clicked`)}
+                 />
+               ))}
+             </div>
+         </div>
+     )
 }
 
 export default Extras
