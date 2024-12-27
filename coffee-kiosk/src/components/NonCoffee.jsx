66d4d7dcfffad8a47ffe61226bed9870dc209dd9
zjsan <zjsantos25@gmail.com>
2024-12-27 09:16:08 +0800

    Update NonCoffee.jsx

--- coffee-kiosk/src/components/NonCoffee.jsx
+++ coffee-kiosk/src/components/NonCoffee.jsx
@@ -1,15 +1,28 @@
+import MenuItem from "./MenuItem"
 
 const NonCoffee = () => {
+
+   const specialsMenu = [
+      { name: "Hot Cholocate", image: "/images/promo-latte.jpg" },
+      { name: "Strawberry", image: "/images/discount-americano.jpg" },
+      { name: "Milk Fudge", image: "/images/discount-americano.jpg" },
+      
+    ];
   return (
-    <div>
-    <h1 className="text-2xl font-bold mb-4 text-center">Non-Coffee</h1>
-    <div className="grid grid-cols-3 gap-4">
-      <div className="border rounded-lg p-4 text-center">Special 1</div>
-      <div className="border rounded-lg p-4 text-center">Special 2</div>
-      <div className="border rounded-lg p-4 text-center">Special 3</div>
-    </div>
-  </div>
-  )
+       <div className="h-screen bg-gray-50">
+           <h1 className="text-2xl font-bold mb-4 text-center">Non-Coffee</h1>
+           <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
+             {specialsMenu.map((item, index) => (
+               <MenuItem
+                 key={index}
+                 name={item.name}
+                 image={item.image}
+                 onClick={() => console.log(`${item.name} clicked`)}
+               />
+             ))}
+           </div>
+       </div>
+   )
 }
 
 export default NonCoffee
