18e8080008992f2a5eaef4abab4be4a5ca5ab241
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2024-12-28 21:18:05 +0800

    Update Footer.jsx

--- coffee-kiosk/src/components/Footer.jsx
+++ coffee-kiosk/src/components/Footer.jsx
@@ -5,12 +5,12 @@ const Footer = () => {
       <div className="text-lg font-bold">
         Total: <span className="text-yellow-400">0.00</span>
       </div>
-      <div className="flex justify-end w-1/2 lg: justify-center"> 
-        <span className="text-sm text-gray-400">Your Order is Empty</span>
-        <button className="bg-yellow-400 hover:bg-yellow-300 text-gray-900 px-4 py-2 rounded ml-2"> 
-          View Your Cart
-        </button>
-      </div>
+      <div className="flex justify-center w-full lg:w-1/2 lg:justify-end"> 
+  <span className="text-sm text-gray-400">Your Order is Empty</span>
+  <button className="bg-yellow-400 hover:bg-yellow-300 text-gray-900 px-4 py-2 rounded ml-2"> 
+    View Your Cart
+  </button>
+</div>
     </footer>
     </div>
    
