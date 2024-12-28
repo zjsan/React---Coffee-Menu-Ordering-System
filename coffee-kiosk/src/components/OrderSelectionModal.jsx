36817bd513fb0eaf13e7227519b0cfb0e1f26ec0
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2024-12-28 21:12:22 +0800

    Update OrderSelectionModal.jsx

--- coffee-kiosk/src/components/OrderSelectionModal.jsx
+++ coffee-kiosk/src/components/OrderSelectionModal.jsx
@@ -9,7 +9,10 @@ const OrderSelectionModal = ({
   price,
   quantity,
   setQuantity,
-  children,
+  type, 
+  setType,
+  size, 
+  setSize
 }) => {
   if (!isOpen) return null;
 
@@ -24,21 +27,92 @@ const OrderSelectionModal = ({
             className="w-24 h-15 rounded-full mb-4 mt-3"
           />
           <p className="text-gray-600 mb-4">{name}</p>
-          <p className="text-xl font-bold mb-6">Price: {price}</p>
-          {children}
-          <form className="form" action="">
-            <div className="flex items-center">
+          <p className="text-xl font-bold mb-3">Price: {price}</p>
+
+          <form className="form mt-0 flex flex-col items-start" action="">
+            {/**Quantity Selection */}
+            <div className="flex items-center"> 
             <span>Quantity: </span>
             <input
               type="number"
               id="quantity"
-              className="mt-1 ml-1 block w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="mt-1 ml-1 block max-w-fit px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               value={quantity}
               onChange={(e) => setQuantity(e.target.value)}
               min="1"
               max="20"
             />
             </div>
+
+          {/* Type Selection */}
+          <div className="mt-0 mb-0 h-1 p-0 flex items-center justify-center py-5 ">
+            <span>Type: </span>
+            <div className="mt-0 ml-1 flex space-x-4">
+              <label>
+                <input
+                  type="radio"
+                  name="type"
+                  value="Iced"
+                  checked={type === "Iced"}
+                  onChange={(e) => setType(e.target.value)}
+                  className="mr-2"
+                />
+                Iced
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="type"
+                  value="Hot"
+                  checked={type === "Hot"}
+                  onChange={(e) => setType(e.target.value)}
+                  className="mr-2"
+                />
+                Hot
+              </label>
+            </div>
+          </div>
+
+          {/* Size Selection */}
+          <div className="mt-0 p-0 h-1 flex items-center justify-center py-5">
+            <span>Size: </span>
+            <div className="mt-1 ml-1 flex space-x-3">
+              <label>
+                <input
+                  type="radio"
+                  name="size"
+                  value="Small"
+                  checked={size === "Small"}
+                  onChange={(e) => setSize(e.target.value)}
+                  className="mr-2"
+                />
+                Small
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="size"
+                  value="Medium"
+                  checked={size === "Medium"}
+                  onChange={(e) => setSize(e.target.value)}
+                  className="mr-2"
+                />
+                Medium
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="size"
+                  value="Large"
+                  checked={size === "Large"}
+                  onChange={(e) => setSize(e.target.value)}
+                  className="mr-2"
+                />
+                Large
+              </label>
+            </div>
+          </div>
+
           </form>
         </div> 
         <div className="flex justify-center flex-col lg:flex-row lg:justify-end mt-5">
