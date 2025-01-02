ad62308554048c823b7c58f134137d0588bbb215
zjsan <zjsantos25@gmail.com>
2025-01-02 09:19:54 +0800

    added ordersSelection and states

--- coffee-kiosk/src/components/Coffee.jsx
+++ coffee-kiosk/src/components/Coffee.jsx
@@ -1,4 +1,6 @@
 import MenuItem from "./MenuItem";
+import { useState } from "react";
+import OrderSelectionModal from "./OrderSelectionModal";
 
 const Coffee = () => {
   const coffeeMenu = [
@@ -10,21 +12,58 @@ const Coffee = () => {
       { name: "Dirty Matcha", image: "/images/discount-americano.jpg" },
     ];
   
-    return (
-      <div className="h-screen bg-gray-50">
-          <h1 className="text-2xl font-bold mb-4 text-center">Coffee</h1>
-          <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
-            {coffeeMenu.map((item, index) => (
-              <MenuItem
-                key={index}
-                name={item.name}
-                image={item.image}
-                onClick={() => console.log(`${item.name} clicked`)}
-              />
-            ))}
-          </div>
+    const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState({ name: "", price: 0, image: "" });
+  const [quantity, setQuantity] = useState(1);
+  const [type, setType] = useState("Iced");
+  const [size, setSize] = useState("Small");
+
+  const openModal = (name, price, image) => {
+    setSelectedItem({ name, price, image });
+    setType("Iced"); // Reset type to default when opening modal
+    setSize("Small"); // Reset size to default when opening modal
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setQuantity(1); // Reset quantity to 1 when opening modal
+    setSelectedItem({ name: "", price: 0, image: "" });
+  };
+
+  return (
+    <div className="h-screen bg-gray-50">
+      <h1 className="text-2xl font-bold mb-4 text-center">Specials</h1>
+      <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
+        {coffeeMenu.map((item, index) => (
+          <MenuItem
+            key={index}
+            name={item.name}
+            price={item.price}
+            image={item.image}
+            onClick={() => openModal(item.name, item.price, item.image)}
+          />
+        ))}
       </div>
-  )
-}
+
+      <OrderSelectionModal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        title="Item Selected"
+        image={selectedItem.image}
+        name={selectedItem.name}
+        price={selectedItem.price}
+        quantity={quantity} 
+        setQuantity={setQuantity}
+        type={type}
+        setType={setType}
+        size={size} 
+        setSize={setSize}
+      >
+      </OrderSelectionModal>
+    </div>
+  );
+};
+
 
 export default Coffee
