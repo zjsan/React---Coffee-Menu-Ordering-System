4349cf05b0ddb6dca451f0fa128d4ba9a6bba5db
zjsan <zjsantos25@gmail.com>
2025-01-02 09:17:37 +0800

    updated price

--- coffee-kiosk/src/components/Coffee.jsx
+++ coffee-kiosk/src/components/Coffee.jsx
@@ -1,10 +1,10 @@
 import MenuItem from "./MenuItem";
 
 const Coffee = () => {
-  const specialsMenu = [
-      { name: "Americano", image: "/images/promo-latte.jpg" },
+  const coffeeMenu = [
+      { name: "Americano", price: 120, image: "/images/promo-latte.jpg" },
       { name: "Caramel Macchiato", price: 150, image: "/images/discount-americano.jpg" },
-      { name: "Spanish Latte", price: 170,  image: "/images/discount-americano.jpg" },
+      { name: "Spanish Latte", price: 160,  image: "/images/discount-americano.jpg" },
       { name: "Mocha", image: "/images/discount-americano.jpg" },
       { name: "Latte", image: "/images/discount-americano.jpg" },
       { name: "Dirty Matcha", image: "/images/discount-americano.jpg" },
@@ -14,7 +14,7 @@ const Coffee = () => {
       <div className="h-screen bg-gray-50">
           <h1 className="text-2xl font-bold mb-4 text-center">Coffee</h1>
           <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
-            {specialsMenu.map((item, index) => (
+            {coffeeMenu.map((item, index) => (
               <MenuItem
                 key={index}
                 name={item.name}
