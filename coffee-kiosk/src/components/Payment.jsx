6facc997f4cef47e7aa7b043991cb0f049ce3100
zjsan <zjsantos25@gmail.com>
2025-01-07 13:42:41 +0800

    Update Payment.jsx

--- coffee-kiosk/src/components/Payment.jsx
+++ coffee-kiosk/src/components/Payment.jsx
@@ -12,6 +12,7 @@ const Payment = () => {
         <h2 className="text-3xl font-bold text-center mb-6">Payment</h2>
         <div>
             <h4 className="text-center">SCAN THE QR CODE TO PAY YOUR ORDERS</h4>
+            <img src="/pictures/qrcode.jpg" alt="GCash QR code image for payment" />
         </div>
         </div>
         
