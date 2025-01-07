b4b5f5e9913180d54e6dd834c855442c19277818
zjsan <zjsantos25@gmail.com>
2025-01-07 11:31:23 +0800

    Create Payment.jsx

---
+++ coffee-kiosk/src/components/Payment.jsx
@@ -0,0 +1,18 @@
+
+import { useNavigate } from "react-router-dom";
+import Header from "./Header";
+const Payment = () => {
+
+
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <Header />
+
+      <div className="container mx-auto py-10 px-4">
+        <h2 className="text-3xl font-bold text-center mb-6">Payment</h2>
+        </div>
+    </div>
+  );
+};
+
+export default Payment;
