040de9d853e63fd2f72cf7075d3f323d42556bb8
zjsan <zjsantos25@gmail.com>
2025-01-09 09:39:24 +0800

    Update Footer.jsx

--- coffee-kiosk/src/components/Footer.jsx
+++ coffee-kiosk/src/components/Footer.jsx
@@ -13,7 +13,11 @@ const Footer = () => {
           Total: <span className="text-yellow-400">{totalPrice.toFixed(2)}</span>
         </div>
         <div className="flex justify-center w-full lg:w-1/2 lg:justify-center">
-          <span className="text-sm text-gray-400">Your Order is Empty</span>
+          {totalPrice.toFixed(2) === "0.00" ? (
+            <span className="text-sm text-gray-400">Your Order is Empty</span>
+          ) : (
+            <span className="text-sm text-gray-400">Check your orders</span>
+          )}
           <button
             className="bg-yellow-400 hover:bg-yellow-300 text-gray-900 px-4 py-2 rounded ml-2"
             onClick={() => navigate("/cart")}
