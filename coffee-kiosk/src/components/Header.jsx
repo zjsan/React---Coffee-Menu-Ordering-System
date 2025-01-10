94fb5386035db99fb3508a22841f3324383b5d65
zjsan <zjsantos25@gmail.com>
2025-01-10 15:00:24 +0800

    Update Header.jsx

--- coffee-kiosk/src/components/Header.jsx
+++ coffee-kiosk/src/components/Header.jsx
@@ -1,9 +1,9 @@
 const Header = () => {
   return (
     <>
-      <header className="bg-gray-800 text-white p-5 flex justify-center items-center text-center mt-0">
-        <h1 className="text-xl font-bold">Coffee Kiosk Pro</h1>
-      </header>
+    <header className="bg-gray-800 text-white flex justify-center items-center text-center mt-0"> 
+  <h1 className="text-xl font-bold">Coffee Kiosk Pro</h1>
+</header>
     </>
   );
 };
