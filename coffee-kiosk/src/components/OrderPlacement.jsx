11d4690fb1add2fa9c9b802b7bf59afdbee9d24b
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-01-12 13:00:02 +0800

    Update OrderPlacement.jsx

--- coffee-kiosk/src/components/OrderPlacement.jsx
+++ coffee-kiosk/src/components/OrderPlacement.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 
 const OrderPlacement = () => {
   const [orderNumber, setOrderNumber] = useState(null);
+  const navigate = useNavigate();
+
   useEffect(() => {
     const storedOrderNumber = localStorage.getItem('orderNumber');
     if (storedOrderNumber) {
@@ -13,19 +15,23 @@ const OrderPlacement = () => {
       localStorage.setItem('orderNumber', newOrderNumber);
     }
   }, []); 
-  const navigate = useNavigate();
-  
+
+  const handleStartNewOrder = () => {
+    localStorage.removeItem('orderNumber'); // Remove the stored order number
+    navigate("/"); 
+  };
+
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center text-gray-900"> 
       <div className="container mx-auto py-10 px-4"> 
         <h2 className="text-3xl font-bold text-center mb-10">Thank you!</h2>
-        <div className="text-center mb-8"> {/* Increased bottom margin */}
+        <div className="text-center mb-8"> 
           <h4 className="mt-5">YOUR ORDER NUMBER IS: </h4>
-          <span className="text-2xl font-bold">{orderNumber}</span> {/* Increased font size */}
-          <p className="mt-4">Present your payment screenshot to the cashier</p> {/* Added top margin to paragraph */}
+          <span className="text-2xl font-bold">{orderNumber}</span> 
+          <p className="mt-4">Present your payment screenshot to the cashier</p> 
         </div>
         <div className="flex flex-col sm:flex-row justify-center space-y-2 sm:space-y-0 sm:space-x-4 mt-8">
-          <button onClick={() => navigate("/")} className="bg-green-500 text-white px-6 py-2 rounded-lg hover:bg-green-600">
+          <button onClick={handleStartNewOrder} className="bg-green-500 text-white px-6 py-2 rounded-lg hover:bg-green-600">
             Start New Order
           </button>
           <button
