5c45f74d511ed5a6a60f3b8c82175406cb9cc13e
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-01-12 12:28:31 +0800

    changed text and background color

--- coffee-kiosk/src/components/Landing.jsx
+++ coffee-kiosk/src/components/Landing.jsx
@@ -4,7 +4,8 @@ const Landing = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="h-screen  flex flex-col justify-center landing">
+    <div className="h-screen  flex flex-col justify-center landing 	
+bg-gray-100 text-gray-900">
       <h1 className="text-2xl font-bold">Welcome to Coffee Kiosk Pro</h1>
       <button 
         className="bg-gray-900 py-4 px-5 border-0 text-white rounded-3xl cursor-pointer start-button" 
