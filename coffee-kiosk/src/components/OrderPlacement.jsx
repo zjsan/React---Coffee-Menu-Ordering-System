78c42400514f28b968fae00d47ee2fb379f9bc64
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2025-01-12 12:52:43 +0800

    Update OrderPlacement.jsx

--- coffee-kiosk/src/components/OrderPlacement.jsx
+++ coffee-kiosk/src/components/OrderPlacement.jsx
@@ -1,6 +1,20 @@
-const OrderPlacement = () => {
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 
-  let orderNumber = Math.floor(Math.random() * 501)
+const OrderPlacement = () => {
+  const [orderNumber, setOrderNumber] = useState(null);
+  useEffect(() => {
+    const storedOrderNumber = localStorage.getItem('orderNumber');
+    if (storedOrderNumber) {
+      setOrderNumber(parseInt(storedOrderNumber)); 
+    } else {
+      const newOrderNumber = Math.floor(Math.random() * 501);
+      setOrderNumber(newOrderNumber);
+      localStorage.setItem('orderNumber', newOrderNumber);
+    }
+  }, []); 
+  const navigate = useNavigate();
+  
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center text-gray-900"> 
       <div className="container mx-auto py-10 px-4"> 
@@ -11,7 +25,7 @@ const OrderPlacement = () => {
           <p className="mt-4">Present your payment screenshot to the cashier</p> {/* Added top margin to paragraph */}
         </div>
         <div className="flex flex-col sm:flex-row justify-center space-y-2 sm:space-y-0 sm:space-x-4 mt-8">
-          <button className="bg-green-500 text-white px-6 py-2 rounded-lg hover:bg-green-600">
+          <button onClick={() => navigate("/")} className="bg-green-500 text-white px-6 py-2 rounded-lg hover:bg-green-600">
             Start New Order
           </button>
           <button
